93c3449908ac7f542e965b4d61e324a7caa12cd7
marcinrabiza <ambrozy.rozek@o2.pl>
2019-06-07 11:47:18 +0200

    Table view for Sylvek

--- RiskManagementTSD/Controllers/HomeController.cs
+++ RiskManagementTSD/Controllers/HomeController.cs
@@ -6,38 +6,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RiskManagementTSD.Models;
 
-namespace RiskManagementTSD.Controllers
+using Microsoft.AspNetCore.Mvc;
+namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("Table");
         }
     }
 }
